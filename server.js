5c3b762fa1d618d525644f23477cbedc5f53b966
leonorader <dernori@gmail.com>
2020-07-12 16:11:08 +0200

    fix endpoints

--- server.js
+++ server.js
@@ -5,21 +5,21 @@ app = express();
 
 let lightBulb = '#303030';
 
-app.get('/', (req, res) => {
+app.get('/api/bulb/color', (req, res) => {
   return res.send(lightBulb);
 });
 
-app.put('/on', (req, res) => {
+app.put('/api/bulb/on', (req, res) => {
   lightBulb = 'yellow'
   return res.sendStatus(200)
 });
 
-app.put('/off', (req, res) => {
+app.put('/api/bulb/off', (req, res) => {
   lightBulb = '#303030'
   return res.sendStatus(200)
 });
 
-app.put('/color', (req, res) => {
+app.put('/api/bulb/color', (req, res) => {
   lightBulb = req.body
   return res.sendStatus(200)
 });
