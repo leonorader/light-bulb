7b545bbeffcfa4d3eb4f43a942c7add314adee4c
leonorader <dernori@gmail.com>
2020-07-12 16:02:25 +0200

    initial

--- server.js
+++ server.js
@@ -3,6 +3,27 @@ var serveStatic = require('serve-static');
 
 app = express();
 
+let lightBulb = '#303030';
+
+app.get('/', (req, res) => {
+  return res.send(lightBulb);
+});
+
+app.put('/on', (req, res) => {
+  lightBulb = 'yellow'
+  return res.sendStatus(200)
+});
+
+app.put('/off', (req, res) => {
+  lightBulb = '#303030'
+  return res.sendStatus(200)
+});
+
+app.put('/color', (req, res) => {
+  lightBulb = req.body
+  return res.sendStatus(200)
+});
+
 app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 3000;
 
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main id="app">
+    <h1></h1>
+    <div :style="{'background-color': lightBulbColor}">
+      <img alt="bulb" src="./assets/bulb-input-dark.png">
+    </div>
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: 'app',
+    computed: {
+      lightBulbColor() {
+        return 'yellow'
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+  #app {
+    background-color: #303030;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
 </style>
